611752b4718f6bfce367318372fdcdb675d566f9
Prasad <bhosaleprasad849@gmail.com>
2023-03-19 11:40:45 +0530

    Change db host

--- db.js
+++ db.js
@@ -2,10 +2,9 @@ const { Pool } = require('pg')
 
 // Replace the values below with your own database credentials
 module.exports =  new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'reunion',
-  password: 'dbqwer',
+  user: 'reunion_6qcf_user',
+  host: 'dpg-cgb0vo02qv267ueii9hg-a',
+  database: 'reunion_6qcf',
+  password: 'zz9WiePMpDUKxGxo7pA2rWMnpZFGt4mi',
   port: 5432,
 })
-
